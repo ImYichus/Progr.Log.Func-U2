6a7f0b9bcac75dd71e71ad5bd5fb25db995bd1e2
YICHUS <157619661+ImYichus@users.noreply.github.com>
2025-03-16 22:57:05 -0600

    Delete Pruebas en Haskell/Bowling directory

--- Pruebas en Haskell/Bowling/app/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import Bowling  -- Importamos todo el módulo Bowling
-
-main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
-  let frames = toFrames [3, 6, 7, 2, 10, 4, 5]  -- Lista de tiros
-  print frames
